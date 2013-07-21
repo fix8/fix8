730be20a909c8c075a6e56787447c972cbb724db
David Dight <dakka@fix8.org>
2013-07-21 16:06:39 +1000

    Upped version

--- version.sh
+++ version.sh
@@ -38,5 +38,5 @@
 #############################################################################################
 MAJOR_VERSION_NUM=0
 MINOR_VERSION_NUM=9
-PATCH_VERSION_NUM=0
+PATCH_VERSION_NUM=2
 echo $MAJOR_VERSION_NUM.$MINOR_VERSION_NUM.$PATCH_VERSION_NUM
