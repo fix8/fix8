dccb3b7ada06e0136866d4e11ef34c1acc7a7b20
sergeys <sedreev@yandex.ru>
2014-12-16 14:34:46 +0300

    fixed win compile errors

--- msvc/nuget/_build-dev.ps1
+++ msvc/nuget/_build-dev.ps1
@@ -1,9 +1,9 @@
 . "$PSScriptRoot\scripts\replace-version.ps1"
 
 # c++ packages
-ReplaceVersion "fix8pro.dev.autopkg" "_1.autopkg"
-ReplaceVersion "fix8pro.dev.tests.autopkg" "_2.autopkg"
-ReplaceVersion "fix8pro.dev.stocklib.autopkg" "_3.autopkg"
+ReplaceVersion "fix8.dev.autopkg" "_1.autopkg"
+ReplaceVersion "fix8.dev.tests.autopkg" "_2.autopkg"
+ReplaceVersion "fix8.dev.stocklib.autopkg" "_3.autopkg"
 Write-NuGetPackage "_1.autopkg"
 Write-NuGetPackage "_2.autopkg"
 Write-NuGetPackage "_3.autopkg"
