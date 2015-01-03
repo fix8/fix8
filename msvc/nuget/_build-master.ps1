1d16a621fccbba4ed5828abc68abf6e7aa175f5c
sergeys <sedreev@yandex.ru>
2015-01-03 11:37:51 +0300

    reverted back release-minor version number since it is used in nuget version for other purpose

--- msvc/nuget/_build-master.ps1
+++ msvc/nuget/_build-master.ps1
@@ -1,7 +1,7 @@
 $RELEASE_MAJOR = 1
 $RELEASE_MINOR = 3
 $RELEASE_DATE = get-date -uformat "%Y%m%d"
-$RELEASE_REVISION = 2
+$RELEASE_REVISION = 1
 (gc fix8.stable.autopkg).replace('${RELEASE_MAJOR}', $RELEASE_MAJOR).replace('${RELEASE_MINOR}', $RELEASE_MINOR).replace('${RELEASE_DATE}', $RELEASE_DATE).replace('${RELEASE_REVISION}', $RELEASE_REVISION) > _1.autopkg
 Write-NuGetPackage _1.autopkg
 (gc fix8.stable.tests.autopkg).replace('${RELEASE_MAJOR}', $RELEASE_MAJOR).replace('${RELEASE_MINOR}', $RELEASE_MINOR).replace('${RELEASE_DATE}', $RELEASE_DATE).replace('${RELEASE_REVISION}', $RELEASE_REVISION) > _2.autopkg
--- msvc/nuget/_publish-dev.ps1
+++ msvc/nuget/_publish-dev.ps1
@@ -5,7 +5,7 @@ param (
 $RELEASE_MAJOR = 1
 $RELEASE_MINOR = 3
 $RELEASE_DATE = get-date -uformat "%Y%m%d"
-$RELEASE_REVISION = 3
+$RELEASE_REVISION = 1
 $API_KEY = $ApiKey
 echo $API_KEY
 Publish-NuGetPackage -ApiKey $API_KEY fix8.dev.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
