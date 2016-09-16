1c0aeb72a04361c146a84afc44af47e8665f0ea1
Sergey S <sedreev@mail.ru>
2016-09-16 15:38:03 +0300

    fixed master nuget package build script

--- msvc/nuget/_publish-master.ps1
+++ msvc/nuget/_publish-master.ps1
@@ -2,15 +2,16 @@ param (
     [string]$ApiKey = "put-your-key-here"
 )
 
-$RELEASE_MAJOR = 1
-$RELEASE_MINOR = 3
+$ver=(gc ..\..\version.sh)
+$RELEASE_MAJOR=[regex]::Match($ver, "MAJOR_VERSION_NUM=(\d+)").Groups[1]
+$RELEASE_MINOR=[regex]::Match($ver, "MINOR_VERSION_NUM=(\d+)").Groups[1]
+$RELEASE_PATCH=[regex]::Match($ver, "PATCH_VERSION_NUM=(\d+)").Groups[1]
 $RELEASE_DATE = get-date -uformat "%Y%m%d"
 $RELEASE_REVISION = 1
+$VERSION="$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_PATCH"
+
 $API_KEY = $ApiKey
 echo $API_KEY
 Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
-Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.symbols.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
 Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.tests.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
-Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.tests.symbols.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
 Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.stocklib.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
-Publish-NuGetPackage -ApiKey $API_KEY fix8.stable.stocklib.symbols.$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_DATE.$RELEASE_REVISION.nupkg
