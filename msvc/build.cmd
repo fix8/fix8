ba4bec2b21b4e0912d3fe36530bfc0d71c14b5ba
Rob Baxter <rbaxter8@gatech.edu>
2017-07-10 23:43:25 -0400

    trying another build

--- msvc/build.cmd
+++ msvc/build.cmd
@@ -1,6 +1,6 @@
 cd msvc
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 msbuild /t:Rebuild /p:Configuration=Release;Platform=x64 /m fix8-vc140.sln
 goto end
 
