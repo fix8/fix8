97dbd26d50aecc1ad37c9dedce40464edd3c6360
sergeys <sedreev@mail.ru>
2019-10-22 19:15:08 +0300

    fixed build script

--- msvc/build-all.cmd
+++ msvc/build-all.cmd
@@ -4,10 +4,10 @@ if not '%1'=='' (
    set Mode=%1
 )
 echo '%Mode% Release x64'
-call build.cmd %Mode% Release x64
+call build.cmd %Mode% Release x64 x64
 echo '%Mode% Debug x64'
-call build.cmd %Mode% Debug x64
+call build.cmd %Mode% Debug x64 x64
 echo '%Mode% Release Win32'
-call build.cmd %Mode% Release Win32
+call build.cmd %Mode% Release Win32 x86
 echo '%Mode% Debug Win32'
-call build.cmd %Mode% Debug Win32
+call build.cmd %Mode% Debug Win32 x86
